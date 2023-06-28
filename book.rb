600f1945b5a53e198274dc4a8c3e3435031c257b
Hernan Zamora <hernanzamora1002@gmail.com>
2023-06-28 10:50:02 -0300

    Created new classes (book, rental, classroom) - Edited old classes (person, student) - Created associations between calsses)

---
+++ book.rb
@@ -0,0 +1,14 @@
+class Book
+  attr_accessor :title, :author, :rentals
+
+  def initialize(title, author)
+    @title = title
+    @author = author
+    @rentals = []
+  end
+
+  def rent_a_book(rental)
+    @rentals.push(rental)
+    rental.book = self
+  end
+end
